ac1684f3783f69ce5919cfe8ebac4a30bae7f7a2
Oussama Moutafatin <moutafatin3@gmail.com>
2023-03-30 22:10:58 +0000

    style input

--- src/components/forms/InputField.vue
+++ src/components/forms/InputField.vue
@@ -18,7 +18,12 @@ export default {
 
 <template>
   <label>
-    <input type="text" v-model="value" v-bind="$attrs" />
+    <input
+      type="text"
+      class="flex h-10 w-full rounded-md border border-slate-300 bg-transparent px-3 py-2 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+      v-model="value"
+      v-bind="$attrs"
+    />
     <p v-show="errorMessage">{{ errorMessage }}</p>
   </label>
 </template>
