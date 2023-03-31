565c463653c0c20c90bec102aa82a46b5616d4d1
Oussama Moutafatin <moutafatin3@gmail.com>
2023-03-31 07:16:33 +0000

    fixing logout api to send cookie

--- src/features/auth/api/logout.ts
+++ src/features/auth/api/logout.ts
@@ -1,5 +1,6 @@
 import { api } from '@/lib/axios'
 
 export function logout() {
-  return api.delete('/auth/logout')
+  return api.delete('/auth/logout', { withCredentials: true })
+  // send cookie with the request, to send the refresh token so the backend can expire it
 }
