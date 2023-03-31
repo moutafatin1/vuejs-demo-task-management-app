c07327b34e7fda12b3e8655c2e9cbba5b8c3144b
Oussama Moutafatin <moutafatin3@gmail.com>
2023-03-31 09:34:20 +0000

    add error type to use query

--- src/features/tasks/pages/TasksPage.vue
+++ src/features/tasks/pages/TasksPage.vue
@@ -1,13 +1,18 @@
 <script lang="ts" setup>
 import { useQuery } from '@tanstack/vue-query'
+import type { AxiosError } from 'axios'
 import { getTasks } from '../api/getTasks'
 import TaskItem from '../components/TaskItem.vue'
 
-const { data, isError, error } = useQuery({ queryKey: ['tasks'], queryFn: getTasks })
+const { data, isError, error } = useQuery({
+  queryKey: ['tasks'],
+  queryFn: getTasks,
+  onError: (err: AxiosError) => err
+})
 </script>
 
 <template>
-  <p v-if="isError">{{ error.message }}</p>
+  <p v-if="isError">{{ error?.message }}</p>
   <h1 class="my-8 text-center text-5xl font-bold text-violet-600">Tasks</h1>
   <ul class="mx-auto max-w-2xl space-y-4 rounded-lg bg-violet-100 p-4">
     <TaskItem v-for="task in data" :task="task" :key="task.id" />
