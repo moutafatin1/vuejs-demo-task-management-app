38c2736d0e25c99e4d654c0651c5d09799a2ad8a
Oussama Moutafatin <moutafatin3@gmail.com>
2023-04-02 13:54:18 +0000

    style text

--- src/components/forms/InputField.vue
+++ src/components/forms/InputField.vue
@@ -20,7 +20,7 @@ export default {
   <label>
     <input
       type="text"
-      class="flex h-10 w-full rounded-md border border-slate-300 bg-transparent px-3 py-2 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+      class="flex h-10 w-full rounded-md border border-slate-300 bg-transparent px-3 py-2 text-sm font-semibold text-gray-700 placeholder:text-slate-400 focus:border-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
       v-model="value"
       v-bind="$attrs"
     />
